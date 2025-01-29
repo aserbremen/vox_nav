cb329d9f6526b7681165e1dcb780c84b650468d1
sero_an <serov@uni-bremen.de>
2025-01-29 14:27:38 +0100

    feat: add action client for planner goal handling in Plan Executor

--- vox_nav_utilities/scripts/plan_executor.py
+++ vox_nav_utilities/scripts/plan_executor.py
@@ -4,6 +4,7 @@ import signal
 
 import rclpy
 from rclpy.node import Node
+from rclpy.action import ActionClient
 from rclpy.callback_groups import ReentrantCallbackGroup
 from rclpy.executors import MultiThreadedExecutor
 
@@ -18,6 +19,7 @@ class PlanExecutor(Node):
         super().__init__('plan_executor')
         self.get_logger().info('Plan Executor Node Started')
         self.namespace = self.declare_parameter('namespace', 'atlas').value
+        self.planner_id = self.declare_parameter('planner_id', 'vox_nav_planning::SE2Planner').value
         # we need a callback group to call backbacks from within callbacks
         self.reentrant_callback_group = ReentrantCallbackGroup()
 
@@ -25,15 +27,36 @@ class PlanExecutor(Node):
         self.goal_pose_sub = self.create_subscription(
             PoseStamped, f'/{self.namespace}/goal_pose', self.goal_pose_callback, 10, callback_group=self.reentrant_callback_group)
 
+        # action clients
+        self.compute_plan_action_topic = f'/{self.namespace}/compute_path_to_pose'
+        self.compute_plan_action_client = ActionClient(
+            self, ComputePathToPose, self.compute_plan_action_topic, callback_group=self.reentrant_callback_group)
+
     def plan_callback(self, msg):
         self.get_logger().info(f'Received Plan: {msg}')
 
     def goal_pose_callback(self, msg: PoseStamped):
         pos = msg.pose.position
         quat = msg.pose.orientation
-        rpy = R.from_quat([quat.x, quat.y, quat.z, quat.w]).as_euler('zyx', degrees=True)
+        rpy = R.from_quat([quat.x, quat.y, quat.z, quat.w]).as_euler('xyz', degrees=True)
         self.get_logger().info(
             f'Received Goal Pose: x = {pos.x}, y = {pos.y}, z = {pos.z}, roll = {rpy[0]}, pitch = {rpy[1]}, yaw = {rpy[2]}')
+
+        print(f'Sending goal to planner on topic {self.compute_plan_action_topic}')
+
+        self.send_planner_goal(msg)
+
+    def send_planner_goal(self, goal_pose: PoseStamped):
+        goal_msg = ComputePathToPose.Goal()
+        goal_msg.pose = goal_pose
+        goal_msg.planner_id = self.planner_id
+
+        self.compute_plan_action_client.wait_for_server(timeout_sec=2.0)
+
+        self.send_goal_future = self.compute_plan_action_client.send_goal_async(goal_msg, feedback_callback=self.plan_callback)
+
+    def plan_callback(self, msg):
+        self.get_logger().info(f'Received Plan: {msg}')
 
 
 def main(args=None):
